e67d747735ec7b69427c57fa2090418a44284ba7
Roberto Viana <robertocarlos@edu.unifor.br>
2025-04-03 21:19:32 -0300

    ✅ Ajusta exibição de mensagens flash no login e recuperação de senha

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,20 +1,25 @@
 # Arquivo: app/routes/auth_routes.py
 # Rotas da interface web relacionadas à autenticação: login, logout e recuperação de senha
 
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, get_flashed_messages
 from app.models.models import Usuario
 from app import db
 from app.forms.login_form import LoginForm
 from app.forms.recuperar_senha_form import RecuperarSenhaForm
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.utils.email_utils import enviar_nova_senha
-from app.utils.resposta import resposta_json
 from .web_routes import web  # Usamos o blueprint "web" unificado para todas as rotas HTML
+#from app.utils.resposta import resposta_json
 
 # ✅ Tela de login – exibe o formulário e processa autenticação
 @web.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    # Limpa mensagens antigas *apenas se* não houver mensagens recentes
+    if request.method == 'GET' and not get_flashed_messages():
+        session.pop('_flashes', None)
+
 
     if form.validate_on_submit():
         email = form.email.data.strip()
@@ -32,10 +37,7 @@ def login():
             flash(f'Bem-vindo, {usuario.nome}!', 'success')
             return redirect(url_for('web.pagina_inicial'))
         else:
-            flash('Email ou senha inválidos.', 'danger')
-
-    # Limpa mensagens antigas na tela de login
-    session.pop('_flashes', None)
+            flash('E-mail ou Senha inválidos.', 'danger')
 
     # Renderiza o formulário com o WTForms
     return render_template('login.html', form=form)
@@ -54,10 +56,19 @@ def recuperar_senha():
 
     if form.validate_on_submit():
         email = form.email.data.strip()
-        # Chama a função de envio de nova senha (já implementada)
-        enviar_nova_senha(email)
-        flash('Uma nova senha foi enviada para seu e-mail.', 'success')
-        return redirect(url_for('web.login'))
+        usuario = Usuario.query.filter_by(email=email).first()
+
+        # print("🔍 MÉTODO:", request.method)
+        # print("✅ FORM VALIDO?", form.validate_on_submit())
+        # print("🧩 E-MAIL:", form.email.data)
+
+        # Chama a função de envio de nova senha (já implementada) se usuário existir
+        if usuario:
+            enviar_nova_senha(email)
+            flash('Nova senha enviada para seu e-mail.', 'success')
+            return redirect(url_for('web.login'))
+        else:
+            flash('E-mail não encontrado.', 'danger')  # Mostra erro claro
 
     return render_template('recuperar_senha.html', form=form)
 
